01aec4cec942080ed90e761b1e3867647a4bb115
Duy Pham <duypham12241999@gmail.com>
2023-02-01 01:40:53 -0800

    make footer stick to bottom

--- file-viewer/src/views/FilesView.vue
+++ file-viewer/src/views/FilesView.vue
@@ -1,11 +1,12 @@
 <template>
+  <v-app-bar title="Simple File Viewer" />
   <v-main>
-    <v-container>
-      <h1>Simple File Viewer</h1>
-    </v-container>
+    <v-container />
     <FileViewer />
   </v-main>
-  <v-footer>
+  <v-footer
+    app
+  >
     <v-container>
       <p>
         Created by <a href="https://duypham.tech/">Duy Pham</a>. Github: <a href="https://github.com/qbit-0/file-viewer-frontend">Frontend</a>,  
