d50dcede668b4076eefce8d02d310e90bbeadfee
Duy Pham <duypham12241999@gmail.com>
2023-01-31 23:45:26 -0800

    improve pdf view

--- file-viewer/src/views/PDFView.vue
+++ file-viewer/src/views/PDFView.vue
@@ -1,7 +1,9 @@
 <template>
-  <iframe 
+  <iframe
+    class="fill-screen"
     width="100%"
     height="100%"
+    frameborder="0"
     :src="`${fullPath}`"
   />
 </template>
@@ -11,10 +13,17 @@ import { computed } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
 
-const fileUrl = `${import.meta.env.VITE_API_URL}/api/file?file_path=${route.query.path}`;
+const fileUrl = `${import.meta.env.VITE_API_URL}/api/file?file_path=${
+  route.query.path
+}`;
 
 const fullPath = computed(() => {
   return `lib/pdfjs-3.3.122-dist/web/viewer.html?file=${fileUrl}`;
 });
-
 </script>
+
+<style>
+.fill-screen {
+  overflow: hidden;
+}
+</style>
